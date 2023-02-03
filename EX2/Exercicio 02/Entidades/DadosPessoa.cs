ed60e92fedeab3e82adf2095b2ad669960d5ab3f
Vinicius Vieira <80359178+vieiravinao@users.noreply.github.com>
2023-02-03 15:10:51 -0300

    Add files via upload

---
+++ EX2/Exercicio 02/Entidades/DadosPessoa.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_02.Entidades
+{
+    public class DadosPessoa
+    {
+        public string Nome;
+
+        public double Salário;
+
+    }
+}
---
+++ EX2/Exercicio 02/MediaSalario.cs
@@ -0,0 +1,23 @@
+﻿
+using Exercicio_02.Entidades;
+
+class MediaSalario {
+        static void Main(string[] args) {
+
+
+        DadosPessoa Funcionario1 = new DadosPessoa();
+        DadosPessoa Funcionario2 = new DadosPessoa();
+
+        Console.Write("Digite o nome do primeiro funcionado:");
+        Funcionario1.Nome = Console.ReadLine();
+        Console.Write("Digite o salário do oreia seca:");
+        Funcionario1.Salário = double.Parse(Console.ReadLine());
+
+        Console.Write("Digite o nome do segundo cabrunco:");
+        Funcionario2.Nome = Console.ReadLine();
+        Console.Write("DIgite o salario do péba:");
+        Funcionario2.Salário= double.Parse(Console.ReadLine());
+
+
+        Console.WriteLine((Funcionario2.Salário + Funcionario1.Salário) / 2);
+} }
